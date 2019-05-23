6d4caa021cc49e597ec01cbe9fca20435d67cb99
Angel Rodríguez <angel_r_m2001@hotmail.com>
2019-05-23 13:43:25 -0500

    modificada la base de datos, Angel tiene que arreglar sus cosas para que funcionen con el nuevo modelo

--- GECO/corpus/models.py
+++ GECO/corpus/models.py
@@ -5,11 +5,13 @@ from .validators import validate_file_extension
 # Create your models here.
 class NormalProject(models.Model):
     name = models.CharField(max_length=100, null=False, unique=True)
+    description = models.CharField(max_length=250, null=False, unique=True)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='owner_normalproject', on_delete=models.CASCADE)
     project_members = models.ManyToManyField(settings.AUTH_USER_MODEL)
     public_status = models.BooleanField(default=0)
     collab_status = models.BooleanField(default=0)
     timestamp = models.DateTimeField(auto_now_add=True)
+
 
     def is_public(self):
         return bool(self.public_status)
@@ -32,11 +34,10 @@ class NormalProject(models.Model):
     def __str__(self):
         return str(self.name)
 
-class Document(models.Model):
+class File(models.Model):
     file = models.FileField(blank=False, null=False, upload_to='mediafiles/', validators=[validate_file_extension])
     name = models.CharField(max_length=100, null=False, unique=True)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='owner_document', on_delete=models.CASCADE)
-    project = models.ForeignKey(NormalProject, related_name='project_document', on_delete=models.CASCADE)
+    document = models.ForeignKey(Document, related_name='file_document', on_delete=models.CASCADE)
     tagged_doc = models.FileField(max_length=100, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
@@ -46,12 +47,16 @@ class Document(models.Model):
     def set_file(self, file_url):
         self.file = file_url
 
+class Document():
+    project = models.ForeignKey(NormalProject, related_name='project_document', on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='owner_document', on_delete=models.CASCADE)
+
 class NormalMetadata(models.Model):
     name = models.CharField(max_length=100, null=False, unique=True)
-    project = models.ForeignKey(NormalProject, related_name='project_normalMetadata', on_delete=models.CASCADE)
+    project = models.ManyToManyField(NormalProject, blank=True)
 
 class DocumentNormalMetadataRelation(models.Model):
     metadata = models.ForeignKey(NormalMetadata, related_name='metadata', on_delete=models.CASCADE)
-    document = models.ForeignKey(Document, related_name='document', on_delete=models.CASCADE)
+    file = models.ForeignKey(File, related_name='file', on_delete=models.CASCADE)
     data = models.CharField(max_length=100, blank=True, null=True)
 

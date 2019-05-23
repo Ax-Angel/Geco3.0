6f390b82ffe821fdaa65e0fb2fafd2914b5906cf
Angel Rodríguez <angel_r_m2001@hotmail.com>
2019-05-23 12:47:45 -0500

    arreglado problema al cargar archivos estaticos y agregadas las primeras vistas html

--- GECO/GECO/settings.py
+++ GECO/GECO/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+#BASE_DIR = '/home/angel/pruebas/Geco3.0/GECO'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
